dcd052194b6de1730f69a63722276dcea021c576
Paulo Gomes <paulorgs.flores@gmail.com>
2021-04-20 22:57:20 -0300

    refactor Welcome screen and add UserIdentification and Confirmation screens

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,33 +1,56 @@
 import React, { useState } from "react";
 import {
   SafeAreaView,
+  View,
   Text,
   Image,
   TouchableOpacity,
   StyleSheet,
   StatusBar,
+  Dimensions,
 } from "react-native";
 
+import { Feather } from "@expo/vector-icons";
+
 import wateringImg from "../assets/watering.png";
-import { Button } from "../components/Button";
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+import { useNavigation } from "@react-navigation/core";
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate("UserIdentification");
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="white"></StatusBar>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>
-          Gerencie {"\n"}
-          suas plantas {"\n"}
-          de forma fácil
-        </Text>
-        <Image source={wateringImg} style={styles.image} />
-        <Text style={styles.subtitle}>
-          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-          sempre que precisar.
-        </Text>
-        <Button title=">" />
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>
+            Gerencie {"\n"}
+            suas plantas de {"\n"}
+            forma fácil
+          </Text>
+          <Image
+            source={wateringImg}
+            style={styles.image}
+            resizeMode="contain"
+          />
+          <Text style={styles.subtitle}>
+            Não esqueça mais de regar suas {"\n"} plantas. Nós cuidamos de
+            lembrar você sempre que precisar.
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+            onPress={handleStart}
+          >
+            <Feather name="chevron-right" style={styles.buttonIcon} />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -36,21 +59,31 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 28,
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
+  },
+
+  image: {
+    height: Dimensions.get("window").width * 0.7,
   },
   button: {
     backgroundColor: colors.green,
@@ -59,14 +92,11 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
+    width: 56,
     paddingHorizontal: 10,
   },
-  image: {
-    width: 292,
-    height: 284,
-  },
-  buttonText: {
+  buttonIcon: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 32,
   },
 });
